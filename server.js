969e2db7db44345f6c037db5eb5ba5888792b29c
Success Ogechi Ezeh <75125594+OgeHub@users.noreply.github.com>
2023-09-03 19:07:46 +0100

    updates

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/public/index.html`);
+  res.send({
+    status: "success",
+    message: "It is working"
+  });
 });
 
 const hostname = "127.0.0.1";
